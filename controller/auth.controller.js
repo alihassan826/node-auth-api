677019fad63cd4fde7e64cc28d6d08c2abaf3235
alihassan826 <alihassan486788@gmail.com>
2023-12-28 00:09:22 +0000

    routes and controllers are done

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,7 +1,77 @@
-exports.login = async () => {
+const user = require('../models/user.model');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userExists = await user.findOne({ email });
+
+        if (!userExists) {
+            return res.status(400).json({
+                message: 'User doesnot exists. Invalid credentials'
+            })
+        }
+
+        const isMatch = bcrypt.compareSync(password, userExists.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Invalid credentials'
+            })
+        };
+
+        const payload = {
+            id: userExists.id
+        };
+
+        jwt.sign(payload, 'myapi', (err, token) => {
+            if (err) {
+                return res.status(400).json({
+                    message: 'Server error'
+                })
+            }
+
+            res.status(200).json({
+                token: token,
+                message: 'User login Succesfully'
+            });
+        });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
-exports.register = async () => {
+exports.register = async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+
+        const userExists = await user.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                message: 'User Already Exists.'
+            })
+        }
+
+        const salt = bcrypt.genSalt(10);
+
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        const newUser = new user({
+            email,
+            password: hashPass
+        });
+
+        await newUser.save();
+
+        res.status(200).json({
+            message: 'Account Created Succusfully'
+        });
+    } catch (err) {
+        res.status(500).send(err)
+    }
 };
